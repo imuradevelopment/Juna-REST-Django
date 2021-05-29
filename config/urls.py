497dddcdce193b2dee6e92d126da1a4a0046fee0
imuradevelopment <imuradevelopmentauth@gmail.com>
2021-05-29 20:30:36 +0900

    モデルの作成

--- config/urls.py
+++ config/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('juna.urls'))
 ]
 
-
-# 開発環境でのメディアファイルの配信設定
 urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
