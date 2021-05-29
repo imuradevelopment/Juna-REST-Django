5b60f917f4f77be1391db68b02c665d31e1ca86a
imuradevelopment <imuradevelopmentauth@gmail.com>
2021-05-29 22:07:53 +0900

    フロントの初期設定完了

--- vue_front/src/views/Home.vue
+++ vue_front/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      ホーム
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 };
 </script>
+
+<style scoped>
+.home{
+    @apply hover:text-red-900;
+}
+</style>
